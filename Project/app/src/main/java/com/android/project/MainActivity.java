c44615a882f0b3382ddb6196d6f0004cce489055
The Anh <nhoxtheanh@gmail.com>
2019-11-16 16:18:06 +0700

    Thêm map & đồng bộ giao diện

--- Project/app/src/main/java/com/android/project/MainActivity.java
+++ Project/app/src/main/java/com/android/project/MainActivity.java
@@ -5,31 +5,30 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import java.util.ArrayList;
 
 
 public class MainActivity extends Activity {
 
-    ListView lv;
-    ArrayList<String> array;
+    private GoogleMap mMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getActionBar().hide();
-        setContentView(R.layout.circle_home);
-        /*
-        lv = (ListView) findViewById(R.id.list_circleMember);
-        array = new ArrayList<>();
-        array.add("Ha Noi");
-        array.add("TP HCM");
-        array.add("Hai Phong");
-        array.add("Can Tho");
-        array.add("Da Nang");
-        array.add("Khanh Hoa");
-        array.add("Nghe An");
+        setContentView(R.layout.login);
 
-        ArrayAdapter adapter = new ArrayAdapter(MainActivity.this,android.R.layout.simple_list_item_1, array);
-<<<<<<< Updated upstream
-        lv.setAdapter(adapter);*/
     }
+
+
 }
