c7b5d8a5c1b195ee4afadb05bc408c62370662d6
1712276_Luong_Hoang_Anh <55074627+hoanganhgo@users.noreply.github.com>
2019-11-18 00:08:11 +0700

    Register simple

--- Project/app/src/main/java/com/android/project/Bussiness.java
+++ Project/app/src/main/java/com/android/project/Bussiness.java
@@ -2,7 +2,6 @@ package com.android.project;
 
 import android.util.Log;
 
-import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -10,11 +9,11 @@ import java.sql.Statement;
 public class Bussiness {
 
     //Nếu thông tin đăng nhập đúng sẽ trả về true
-    public boolean login(String userName, String passWord, Connection connection)
+    public static boolean login(String userName, String passWord)
     {
         try {
             //truy xuất cơ sở dữ liệu sql
-            Statement statement=connection.createStatement();
+            Statement statement=MainActivity.connection.createStatement();
             ResultSet resultSet=statement.executeQuery("Select PassWord From Account Where UserName="+"'"+userName+"'");
             resultSet.next();
             //Lấy mật khẩu tương ứng với username
@@ -27,6 +26,28 @@ public class Bussiness {
                 return false;
             }
         } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean register(String userName, String passWord)
+    {
+        try {
+            //truy xuất cơ sở dữ liệu sql
+            Statement statement=MainActivity.connection.createStatement();
+            //Chọn ID cho tài khoản mới
+            ResultSet resultSet=statement.executeQuery("Select MAX(ID_Account) from Account");
+            resultSet.next();
+            int newID=resultSet.getInt(1)+1;
+            //Đưa dữ liệu lên Database
+            statement.executeUpdate("Insert into Account (ID_Account, UserName, PassWord, Coordinates_X, Coordinates_Y, RealTime, Battery, Speed, Share_Location, Share_Battery, Share_Speed) "
+                    + " values ("+newID+", '"+userName+"', '"+passWord+"', 12.2, 106.78, '2019-11-17 23:00:00', 100, 120, 1 , 1, 1)");
+           // resultSet.next();
+            Log.e("hoanganh","Insert success!");
+            return true;
+        } catch (SQLException e) {
+            Log.e("hoanganh","Insert fail");
             e.printStackTrace();
             return false;
         }
--- Project/app/src/main/java/com/android/project/MainActivity.java
+++ Project/app/src/main/java/com/android/project/MainActivity.java
@@ -2,6 +2,7 @@ package com.android.project;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -16,27 +17,26 @@ public class MainActivity extends Activity {
 
     private GoogleMap mMap;
     private ConnectionHelper connectionHelper;
-    private Connection connection=null;
-    private Bussiness bussiness;
+    public static Connection connection=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getActionBar().hide();
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_main);
+       // getActionBar().hide();
         //Khởi tạo lớp Bussiness
-        bussiness=new Bussiness();
         //Kết nối cơ sở dữ liệu
         connectionHelper = new ConnectionHelper();
         connection = connectionHelper.connectToServer();
+        setContentView(R.layout.login);
     }
 
 
     public void login_Click(View view) {
         EditText edit_user=(EditText)findViewById(R.id.inputUser);
         EditText edit_pass=(EditText)findViewById(R.id.inputPass);
-        boolean loginStatus = bussiness.login(edit_user.getText().toString(),edit_pass.getText().toString(),connection);
-        if (loginStatus==true)
+        boolean loginStatus = Bussiness.login(edit_user.getText().toString(),edit_pass.getText().toString());
+        if (loginStatus)
         {
             setContentView(R.layout.circle_home);
         }
@@ -44,5 +44,10 @@ public class MainActivity extends Activity {
             TextView login_status=(TextView)findViewById(R.id.loginStatus);
             login_status.setText("Username or Password incorrect!");
         }
+    }
+
+    public void register_Click(View view) {
+        Intent register=new Intent(this,Register_Activity.class);
+        startActivity(register);
     }
 }
