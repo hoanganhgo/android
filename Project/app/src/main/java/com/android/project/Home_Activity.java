d322eeefa3b148f67d7c9085ab5f3dd1ae04ad21
1712276_Luong_Hoang_Anh <55074627+hoanganhgo@users.noreply.github.com>
2019-11-18 23:15:32 +0700

    Update XML

--- Project/app/src/main/java/com/android/project/Home_Activity.java
+++ Project/app/src/main/java/com/android/project/Home_Activity.java
@@ -2,13 +2,19 @@ package com.android.project;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 
 public class Home_Activity extends Activity {
+    ListView listView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.circle_home);
+
+        listView = (ListView) findViewById(R.id.listCircle);
+
+
     }
 }
